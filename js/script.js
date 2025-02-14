797dd04df5fb4fdf5c012e288506eb21e22ec754
rrraayyy <ray941005@gmail.com>
2025-02-14 10:02:33 +0800

    換頁測試-2

--- js/script.js
+++ js/script.js
@@ -1,31 +1,33 @@
-    function loadPage() {
-      const content = document.getElementById("content");
-      const hash = window.location.hash;
-  
-      // 動態載入不同頁面的 HTML
-      if (hash === "#board") {
-        fetch('../board.html')
-          .then(response => response.text())
-          .then(data => {
-            content.innerHTML = data;
-          });
-      } else if (hash === "#events") {
-        fetch('../events.html')
-          .then(response => response.text())
-          .then(data => {
-            content.innerHTML = data;
-          });
-      } else if (hash === "#story") {
-        fetch('story.html')
-          .then(response => response.text())
-          .then(data => {
-            content.innerHTML = data;
-          });
-      } else {
-        content.innerHTML = document.querySelector("#content").innerHTML;
-      }
-    }
-  
-    // 監聽 hash 變化，根據網址變更內容
-    window.addEventListener("hashchange", loadPage);
-    window.addEventListener("load", loadPage);
+const originalContent = document.getElementById("content").innerHTML;
+
+function loadPage() {
+  const content = document.getElementById("content");
+  const hash = window.location.hash;
+
+  // 動態載入不同頁面的 HTML
+  if (hash === "#board") {
+    fetch('../board.html')
+      .then(response => response.text())
+      .then(data => {
+        content.innerHTML = data;
+      });
+  } else if (hash === "#events") {
+    fetch('../events.html')
+      .then(response => response.text())
+      .then(data => {
+        content.innerHTML = data;
+      });
+  } else if (hash === "#story") {
+    fetch('story.html')
+      .then(response => response.text())
+      .then(data => {
+        content.innerHTML = data;
+      });
+  } else {
+    content.innerHTML = originalContent;
+  }
+}
+
+// 監聽 hash 變化，根據網址變更內容
+window.addEventListener("hashchange", loadPage);
+window.addEventListener("load", loadPage);
